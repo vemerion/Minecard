ca56d4628443d3df18b9a8ce397828f8cff63763
vemerion <vemerion@gmail.com>
2021-08-14 19:32:51 +0200

    Update Forge version to fix RenderHandEvent

--- src/main/java/mod/vemerion/minecard/eventsubscriber/RenderCard.java
+++ src/main/java/mod/vemerion/minecard/eventsubscriber/RenderCard.java
@@ -14,7 +14,6 @@ import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.HumanoidArm;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderHandEvent;
@@ -39,26 +38,25 @@ public class RenderCard {
 				: player.getMainArm().getOpposite();
 		float offset = bothArms ? 0 : (arm == HumanoidArm.LEFT ? -1 : 1);
 		float xProgress = bothArms ? Mth.clamp(player.getXRot(), 0, 30) / 30 : 1;
+		float equipProgress = event.getEquipProgress();
+		float swingProgress = event.getSwingProgress();
 		PlayerRenderer renderer = (PlayerRenderer) mc.getEntityRenderDispatcher().getRenderer(player);
 		PoseStack pose = event.getMatrixStack();
 		pose.pushPose();
-		pose.translate(0,
-				-0.4 + 0.3 * xProgress - getEquipProgress(event)
-						- (bothArms ? Mth.sin(getSwingProgress(player, event) * Mth.PI) : 0),
-				-0.7 - (bothArms ? Mth.sin(getSwingProgress(player, event) * Mth.PI) : 0));
+		pose.translate(0, -0.4 + 0.3 * xProgress - equipProgress - (bothArms ? Mth.sin(swingProgress * Mth.PI) : 0),
+				-0.7 - (bothArms ? Mth.sin(swingProgress * Mth.PI) : 0));
 
 		// Render hand(s)
 		pose.pushPose();
 		pose.translate(offset, -0.8, 0.4);
-		pose.mulPose(new Quaternion(-50, 0, offset * Mth.sin(getSwingProgress(player, event)) * 80, true));
+		pose.mulPose(new Quaternion(-50, 0, offset * Mth.sin(swingProgress) * 80, true));
 		renderArm(arm, renderer, pose, event.getBuffers(), event.getLight(), player);
 		if (bothArms)
 			renderArm(arm.getOpposite(), renderer, pose, event.getBuffers(), event.getLight(), player);
 		pose.popPose();
 
 		// Render card
-		pose.translate(offset * 0.6 - offset * Mth.sin(getSwingProgress(player, event)),
-				bothArms ? 0 : -Mth.sin(getSwingProgress(player, event)) * 2, 0);
+		pose.translate(offset * 0.6 - offset * Mth.sin(swingProgress), bothArms ? 0 : -Mth.sin(swingProgress) * 2, 0);
 		pose.mulPose(new Quaternion(80 * xProgress - 90, 0, 0, true));
 		mc.getItemRenderer().renderStatic(null, stack, TransformType.NONE, false, event.getMatrixStack(),
 				event.getBuffers(), null, event.getLight(), OverlayTexture.NO_OVERLAY, 0);
@@ -77,19 +75,5 @@ public class RenderCard {
 		else if (arm == HumanoidArm.RIGHT)
 			renderer.renderRightHand(pose, buffers, light, player);
 		pose.popPose();
-	}
-
-	// TODO: Change this when Forge bug is fixed
-	private static float getEquipProgress(RenderHandEvent event) {
-		if (event.getHand() == InteractionHand.MAIN_HAND)
-			return 0;
-		return event.getEquipProgress();
-	}
-
-	// TODO: Change this when Forge bug is fixed
-	private static float getSwingProgress(Player player, RenderHandEvent event) {
-		if (event.getHand() == InteractionHand.OFF_HAND)
-			return event.getSwingProgress();
-		return player.swingingArm == event.getHand() ? player.getAttackAnim(event.getPartialTicks()) : 0;
 	}
 }
